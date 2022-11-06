6742e16679db5e3885da8f6fd312efe51c596ded
pavan <pavan.dhadange@gmail.com>
2022-11-06 10:59:47 +0530

    Added unit test case for User registration controller

--- src/main/java/com/synchrony/codingchallenege/constants/AppConstants.java
+++ src/main/java/com/synchrony/codingchallenege/constants/AppConstants.java
@@ -3,7 +3,8 @@ package com.synchrony.codingchallenege.constants;
 public class AppConstants {
 
 	
-	public static final String USER_ID = "USERID";
+	public static final String USER_ID = "userId";
+	public static final String PASSWORD = "password";
 	
 	private AppConstants() {}
 	
---
+++ src/test/java/com/synchrony/codingchallenege/controller/UserRegistrationControllerTest.java
@@ -0,0 +1,53 @@
+package com.synchrony.codingchallenege.controller;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import com.synchrony.codingchallenege.dto.UserDTO;
+import com.synchrony.codingchallenege.repository.ImageRepository;
+import com.synchrony.codingchallenege.repository.UserRepository;
+import com.synchrony.codingchallenege.service.UserRegistrationService;
+
+
+import static org.mockito.Mockito.*;
+
+import java.net.URI;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserRegistrationController.class)
+//@SpringBootTest
+public class UserRegistrationControllerTest {
+
+	
+	@Autowired
+	MockMvc mvc;
+	
+	@MockBean
+	private UserRegistrationService userService;
+	
+	@MockBean
+	UserRepository userRepository;
+	
+	@MockBean
+	ImageRepository imageRepository;
+	
+	@Test
+	public void fetchUserInfoTest() throws Exception {
+		UserDTO user = new UserDTO();
+		user.setUserId("XYZ");
+		when(userService.getUserInfo(anyString())).thenReturn(user);
+		
+		mvc.perform(MockMvcRequestBuilders
+				.get(URI.create("/userprofile/api/v1/users/XYZ"))
+				.accept(MediaType.APPLICATION_JSON_VALUE)
+				)
+		.andExpect(status().isOk());
+	}
+}
